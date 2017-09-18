74202e4deb730eaa17b3b25a3482ce49dc3507d7
ohrstrom <ohrstrom@gmail.com>
2017-09-18 19:32:50 +0200

    preparing for distribution/testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 INSTALL_REQUIREMENTS = [
     'Click>=6.0,<7.0',
-    'click_log>=2.0,<3.0',
+    'click_log>=0.2.0,<0.3.0',
     'requests>=2.9.1',
     'APScheduler>=3.3.1,<4.0',
 ]
